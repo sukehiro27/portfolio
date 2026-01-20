eea321420afd6048e94096ff34590b2e53cbdf56
sukehiro27 <kiralyllanda@gmail.com>
2026-01-20 18:56:23 +0800

    initialize

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -18,21 +18,51 @@
       <UContainer>
         <div class="flex">
           <div class="flex-1 text-left">
-            <h1 class="font-bold text-3xl pt-20 mb-5">---About Me---</h1>
+            <h1 class="font-bold text-3xl mt-40 mb-15">---About Me---</h1>
             <p class="font-semibold text-2xl">Hi, I'm Kiraly</p>
-            <p>I'm a junior wed developer specializing in creating clean and responsive websites.
+            <p>I'm a junior web developer specializing in creating clean and responsive websites.
               I have passion for writing code that is simple yet effective.
             </p>
             <p class="pt-5">I enjoy learning the latest front-end and back-end technologies to build practical web appplications, and continually improve my skills.</p>
           </div>
 
           <div class="flex-1">
-            <div>
+            <div class="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
               <UCard class="ms-20 mt-35 rounded-xl shadow-lg">
                 <UIcon name="i-lucide-user-round" class="w-10 h-10 absolute" />
                 <div class="flex flex-col ms-15">
                   <h1 class="text-muted">Full Name</h1>
                   <p class="font-semibold">Kiraly Llanda</p>
+                </div>
+              </UCard>
+            </div>
+
+            <div class="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+              <UCard class="ms-20 mt-3 rounded-xl shadow-lg">
+                <UIcon name="i-lucide-code" class="w-10 h-10 absolute" />
+                <div class="flex flex-col ms-15">
+                  <h1 class="text-muted">Role</h1>
+                  <p class="font-semibold">Junior Web Developer</p>
+                </div>
+              </UCard>
+            </div>
+
+            <div class="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+              <UCard class="ms-20 mt-3 rounded-xl shadow-lg">
+                <UIcon name="i-lucide-map-pin" class="w-10 h-10 absolute" />
+                <div class="flex flex-col ms-15">
+                  <h1 class="text-muted">Location</h1>
+                  <p class="font-semibold">Philippines</p>
+                </div>
+              </UCard>
+            </div>
+
+            <div class="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+              <UCard class="ms-20 mt-3 rounded-xl shadow-lg">
+                <UIcon name="i-lucide-briefcase-business" class="w-10 h-10 absolute" />
+                <div class="flex flex-col ms-15">
+                  <h1 class="text-muted">Experience</h1>
+                  <p class="font-semibold">Entry Level</p>
                 </div>
               </UCard>
             </div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
     <div>
       <UHeader title="" toggle-side="right">
         <UNavigationMenu :items="items" class="w-full justify-center" :ui="{ label: 'active:text-blue-500' }" />
-
+        
         <template #right>
           <UColorModeButton />
         </template>
@@ -16,10 +16,14 @@
     </div>
     <div class="bg-gray-50 dark:bg-gray-900 min-h-screen">
       <UContainer>
-        <div class="flex justify-between ">
-          <div>
+        <div class="flex flex-col md:flex-row items-center justify-between">
+          <div
+            class="order-1 md:order-2 mx-auto sm:mx-0 w-70 h-70 sm:w-70 md:w-90 sm:h-70 md:h-90 rounded-2xl bg-blue-50 dark:bg-gray-800 overflow-hidden mt-17">
+            <img src="/assets/formal1.png" alt="picture" class="object-cover w-full h-full transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+          </div>
+          <div class="order-2 md:order-1">
             <p
-              class="w-full text-lg ms-5 sm:ms-10 md:ms-15 sm:text-2xl md:text-3xl mt-20 sm:mt-40 text-gray-500 dark:text-white">
+              class="w-full text-lg ms-5 sm:ms-10 md:ms-15 sm:text-2xl md:text-3xl pt-20 sm:pt-40 text-gray-500 dark:text-white">
               Hi, I'm Kiraly</p>
             <p class="w-full text-xl sm:text-3xl md:text-5xl font-bold ms-5 sm:ms-10 md:ms-15 mt-2 sm:mt-5">Junior Web
             </p>
@@ -28,57 +32,56 @@
               website
               and web applications.</p>
             <div class="flex">
-              <UButton
-                class="cursor-pointer mt-10 sm:mt-6 ms-5 sm:ms-15 md:ms-20 p-1 sm:p-4 text-xs sm:text-sm rounded-xl transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 bg-blue-500 hover:bg-blue-300 shadow-lg dark:shadow-xl">
+              <UButton class="cursor-pointer justify-center w-40 h-13 mt-10 sm:mt-6 ms-2 sm:ms-15 md:ms-20 sm:p-4 text-sm sm:text-sm rounded-xl transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 bg-blue-500 hover:bg-blue-300 shadow-lg dark:shadow-xl">
                 View My Project</UButton>
-              <UButton
-                class="cursor-pointer mt-10 sm:mt-6 ms-2 sm:ms-10 md:ms-5 p-1 sm:p-4 text-xs sm:text-sm rounded-xl transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 bg-gray-400 hover:bg-gray-300 shadow-lg dark:shadow-xl">
+              <UButton class="cursor-pointer justify-center w-40 h-13 mt-10 sm:mt-6 ms-2 sm:ms-10 md:ms-5 sm:p-4 text-sm sm:text-sm rounded-xl transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 bg-gray-400 hover:bg-gray-300 shadow-lg dark:shadow-xl">
                 <UIcon name="i-lucide-send" />Contact Me
               </UButton>
             </div>
           </div>
 
-          <div class="w-80 h-50 sm:w-70 md:w-90 sm:h-70 md:h-90 rounded-2xl bg-blue-50 dark:bg-gray-800 overflow-hidden mt-17 ms-10">
-            <img src="/assets/formal1.png" alt="picture" class="object-cover w-full h-full transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
-          </div>
         </div>
 
-        <div class="mt-35 flex-col-2 sm:flex">
+        <div class="mt-35">
           <p class="font-bold text-xl ms-5 sm:ms-10 md:ms-15">Skills</p>
-          <div class="skill-card opacity-0 translate-y-6 transition-all duration-700 ease-out">
-            <UCard class="w-70 ms-5 mt-15 h-70 hover:-translate-y-1 hover:scale-110 transition">
-              <p class="font-bold text-xl text-center">Front-end</p>
-              <USeparator />
-              <section class="mt-5">
-                <p class="font-semibold">HTML</p>
-                <p class="font-semibold">CSS</p>
-                <p class="font-semibold">Javascript</p>
-              </section>
-            </UCard>
-          </div>
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 place-items-center mt-10">
 
-          <div class="skill-card opacity-0 translate-y-6 transition-all duration-700 ease-out">
-            <UCard class="w-70 ms-5 mt-15 h-70 hover:-translate-y-1 hover:scale-110 transition">
-              <p class="font-bold text-xl text-center">Back-end</p>
-              <USeparator />
-              <section class="mt-5">
-                <p class="font-semibold">PHP</p>
-                <p class="font-semibold">Firebase</p>
-              </section>
-            </UCard>
-          </div>
+            <div class="skill-card opacity-0 translate-y-6 transition-all duration-700 ease-out">
+              <UCard class="w-70 h-70 hover:-translate-y-1 hover:scale-110 transition">
+                <p class="font-bold text-xl text-center">Front-end</p>
+                <USeparator />
+                <section class="mt-5 space-y-1">
+                  <p class="font-semibold">HTML</p>
+                  <p class="font-semibold">CSS</p>
+                  <p class="font-semibold">Javascript</p>
+                </section>
+              </UCard>
+            </div>
+    
+            <div class="skill-card opacity-0 translate-y-6 transition-all duration-700 ease-out">
+              <UCard class="w-70 h-70 hover:-translate-y-1 hover:scale-110 transition">
+                <p class="font-bold text-xl text-center">Back-end</p>
+                <USeparator />
+                <section class="mt-5 space-y-1">
+                  <p class="font-semibold">PHP</p>
+                  <p class="font-semibold">Firebase</p>
+                </section>
+              </UCard>
+            </div>
 
-          <div class="skill-card opacity-0 translate-y-6 transition-all duration-700 ease-out">
-            <UCard class="w-70 ms-5 mt-15 h-70 hover:-translate-y-1 hover:scale-110 transition">
-              <p class="font-bold text-xl text-center">Tools</p>
-              <USeparator />
-              <section class="mt-5">
-                <p class="font-semibold">Bootstrap</p>
-                <p class="font-semibold">Vue</p>
-                <p class="font-semibold">Nuxt js</p>
-                <p class="font-semibold">Tailwindcss</p>
-              </section>
-            </UCard>
+            <div class="skill-card opacity-0 translate-y-6 transition-all duration-700 ease-out">
+              <UCard class="w-70 h-70 hover:-translate-y-1 hover:scale-110 transition">
+                <p class="font-bold text-xl text-center">Tools</p>
+                <USeparator />
+                <section class="mt-5 space-y-1">
+                  <p class="font-semibold">Bootstrap</p>
+                  <p class="font-semibold">Vue</p>
+                  <p class="font-semibold">Nuxt js</p>
+                  <p class="font-semibold">Tailwindcss</p>
+                </section>
+              </UCard>
+            </div>
+
           </div>
         </div>
       </UContainer>
@@ -134,7 +137,7 @@ const items = ref<NavigationMenuItem[][]>([
       class: ' transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer',
       label: 'GitHub',
       icon: 'i-simple-icons-github',
-      badge: 'NEW',
+      badge: 'Click',
       to: 'https://github.com/sukehiro27',
       target: '_blank'
     }
